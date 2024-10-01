e88ca14ac66ebbbbeedace98fd9c8176609572f3
Endkn <kenichi_endo@mail.toyota.co.jp>
2024-10-01 11:53:53 +0900

    Create images.js

---
+++ images.js
@@ -0,0 +1,16 @@
+'use strict'
+// Please don't delete the 'use strict' line above
+
+const images = [
+    {src: "./picture/smile_man.png", description: "smile_man_150"},
+    {src: "./picture/thanks_man.png", description: "thanks_man_310"},
+    {src: "./picture/depressed_woman.png", description: "depressed_woman_397"},
+    {src: "./picture/surprised_man.png", description: "Surprize_man_402"},
+    {src: "./picture/hi_man.png", description: "hi_man_412"},
+    {src: "./picture/relax_man.png", description: "relax_man_560"},
+    {src: "./picture/regrettable_man.png", description: "regrettable_man_590"},
+    {src: "./picture/wow_woman.png", description: "wow_woman_740"},
+    {src: "./picture/fear_man.png", description: "fear_man_893"},
+    {src: "./picture/pray_man.png", description: "pray_man_895"},
+    {src: "./picture/proudly_man.png", description: "proudly_man_720"},
+]
